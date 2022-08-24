3438f2e95b615a25b51f5b46ac7aed30ec7605a7
miozune <miozune@gmail.com>
2022-08-24 15:00:44 +0900

    Switch to GTNH ForgeGradle

--- src/main/java/com/fireball1725/defaultworldgenerator/DefaultWorldGenerator.java
+++ src/main/java/com/fireball1725/defaultworldgenerator/DefaultWorldGenerator.java
@@ -4,7 +4,6 @@ import com.fireball1725.defaultworldgenerator.config.ConfigGeneralSettings;
 import com.fireball1725.defaultworldgenerator.config.ConfigurationFile;
 import com.fireball1725.defaultworldgenerator.events.GuiEvents;
 import com.fireball1725.defaultworldgenerator.lib.Log;
-import com.fireball1725.defaultworldgenerator.lib.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -12,7 +11,7 @@ import net.minecraft.world.WorldType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.config.Configuration;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION_BUILD)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class DefaultWorldGenerator {
     @Mod.Instance
     public static DefaultWorldGenerator instance;
